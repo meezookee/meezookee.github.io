e7acdf599dd98d122547dd8e28e47f1f6856b3ee
meezookee <109216261+meezookee@users.noreply.github.com>
2026-04-16 20:51:24 +0900

    add feed. getting messy

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,3 +1,5 @@
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
 export default function (eleventyConfig) {
   eleventyConfig.addTemplateFormats("txt");
   eleventyConfig.addExtension("txt", {
@@ -22,4 +24,20 @@ export default function (eleventyConfig) {
   );
 
   eleventyConfig.addFilter("toISOString", (v) => v.toISOString());
+
+  eleventyConfig.addPlugin(feedPlugin, {
+    collection: {
+      name: "post",
+    },
+    metadata: {
+      language: "ja",
+      title: "閉塞圏",
+      subtitle: "閉塞圏",
+      base: "https://meezookee.github.io",
+      author: {
+        name: "meezookee",
+        email: "meezookee@example.invalid",
+      },
+    },
+  });
 }
